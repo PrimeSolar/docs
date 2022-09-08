fc5848a41a834cf42e6e064cb1b0306f4585b0d5
Sony <88476416+FirstSolar@users.noreply.github.com>
2022-09-08 17:20:09 +0300

    Create elements.js

---
+++ scripts/elements.js
@@ -0,0 +1,12 @@
+// Footer
+footerContainer = document.querySelector("#footerContainer");
+function footerContainerF(footerContainer) {
+  if (footerContainer != null) {
+    footerContainer.innerHTML += `
+<footer>
+Copyright © Real&Mate 2022
+</footer>
+      `
+  }
+}
+footerContainerF(footerContainer);

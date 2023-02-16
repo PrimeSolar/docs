016336e265d712269616601849592124606feab0
Sony <88476416+FirstSolar@users.noreply.github.com>
2023-02-16 22:37:52 +0300

    Update editor-mode.js

--- scripts/editor-mode.js
+++ scripts/editor-mode.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function editorMode() {
   if (document.documentElement.classList.contains("editor-mode-on")){
     document.documentElement.classList.remove("editor-mode-on")
     document.documentElement.classList.add("editor-mode-off")
@@ -12,11 +12,11 @@ function myFunction() {
       alert("Now you can edit this document in your browser.\nIMPORTANT: changes will not be saved on the server.\nYou may save edited document on your device as an HTML page for later usage.");
         document.documentElement.classList.add("editor-mode-on-alerted")
     }
-  };
+  }
 }
 
-const editorMode = document.getElementById('editor-mode');
+const constEditorMode = document.getElementById('editor-mode');
 function moveIndicator() {
-  editorMode.classList.toggle('editIsActive');
+  constEditorMode.classList.toggle('editIsActive');
 }
-editorMode.addEventListener("click", moveIndicator, false);
+constEditorMode.addEventListener("click", moveIndicator, false);

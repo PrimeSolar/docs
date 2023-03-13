fb0ddda0cc471a043699f8f27ec4655d69a28dca
Sony <88476416+FirstSolar@users.noreply.github.com>
2023-03-14 00:01:20 +0300

    Update display.js
    Refactor and beautify source code

--- scripts/display.js
+++ scripts/display.js
@@ -1,25 +1,35 @@
-  var userRole = prompt("Please enter the role. Options:\nadmin (default)\nuser", "admin");
-  var subtitle = document.getElementById("subtitle");
-  if (userRole == "user") {
-    subtitle.innerHTML = "for Users";
-    var el = document.getElementsByClassName("no-user-section");
-    for (x = 0; x < el.length; x++) {
-      el[x].closest("section").style.display = "none";
-    }
-    var el2 = document.getElementsByClassName("no-user-details");
-    for (y = 0; y < el2.length; y++) {
-      el2[y].closest("details").style.display = "none";
-    }
-  } else {
-    subtitle.innerHTML = "for Administrators";
-    var el = document.getElementsByClassName("no-admin");
-    for (x = 0; x < el.length; x++) {
-      el[x].closest("section").style.display = "none";
-    }
-    var el2 = document.getElementsByClassName("no-admin-details");
-    for (y = 0; y < el2.length; y++) {
-      el2[y].closest("details").style.display = "none";
-    }
+var userRole = prompt("Please enter the role. Options:\nuser (default)\nadmin", "user");
+var subtitle = document.getElementById("subtitle");
+
+function displayForAdmin() {
+  subtitle.innerHTML = "for Administrators";
+  var el = document.getElementsByClassName("no-admin");
+  for (x = 0; x < el.length; x++) {
+    el[x].closest("section").style.display = "none";
   }
-  var script = document.getElementsByTagName("script");
-  script[0].innerHTML = "";
+  var el2 = document.getElementsByClassName("no-admin-details");
+  for (y = 0; y < el2.length; y++) {
+    el2[y].closest("details").style.display = "none";
+  }
+}
+
+function displayForUser() {
+  subtitle.innerHTML = "for Users";
+  var el = document.getElementsByClassName("no-user-section");
+  for (x = 0; x < el.length; x++) {
+    el[x].closest("section").style.display = "none";
+  }
+  var el2 = document.getElementsByClassName("no-user-details");
+  for (y = 0; y < el2.length; y++) {
+    el2[y].closest("details").style.display = "none";
+  }
+}
+
+if (userRole == "admin") {
+  displayForAdmin()
+} else {
+  displayForUser()
+}
+
+var script = document.getElementsByTagName("script");
+script[0].innerHTML = "";

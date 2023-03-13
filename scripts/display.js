f0bce6c79c680d0e49c93411e1190b0e7ffffa7d
Sony <88476416+FirstSolar@users.noreply.github.com>
2023-03-13 11:42:37 +0300

    Update display.js
    Design the details section display setting

--- scripts/display.js
+++ scripts/display.js
@@ -2,9 +2,13 @@
   var subtitle = document.getElementById("subtitle");
   if (userRole == "user") {
     subtitle.innerHTML = "for Users";
-    var el = document.getElementsByClassName("no-user");
+    var el = document.getElementsByClassName("no-user-section");
     for (x = 0; x < el.length; x++) {
       el[x].closest("section").style.display = "none";
+    }
+    var el2 = document.getElementsByClassName("no-user-details");
+    for (y = 0; y < el2.length; y++) {
+      el2[y].closest("details").style.display = "none";
     }
   } else {
     subtitle.innerHTML = "for Administrators";
@@ -12,6 +16,10 @@
     for (x = 0; x < el.length; x++) {
       el[x].closest("section").style.display = "none";
     }
-  };
+    var el2 = document.getElementsByClassName("no-admin-details");
+    for (y = 0; y < el2.length; y++) {
+      el2[y].closest("details").style.display = "none";
+    }
+  }
   var script = document.getElementsByTagName("script");
   script[0].innerHTML = "";

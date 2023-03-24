2165343a60f969be6b780c6a10be1f2ff61601be
Sony <88476416+FirstSolar@users.noreply.github.com>
2023-03-24 22:09:28 +0300

    Update display.js
    Expand and enhance document display algorithm depending on the role

--- scripts/display.js
+++ scripts/display.js
@@ -1,34 +1,42 @@
-var userRole = prompt("Please enter the role. Options:\nuser (default)\nadmin", "user");
+var userRole = prompt("Please enter the role. Options:\nadmin (default)\nuser", "admin");
 var subtitle = document.getElementById("subtitle");
 
 function displayForAdmin() {
   subtitle.innerHTML = "for Administrators";
   var el = document.getElementsByClassName("no-admin");
-  for (x = 0; x < el.length; x++) {
-    el[x].closest("section").style.display = "none";
+  for (let x = 0; x < el.length; x++) {
+    el[x].style.display = "none";
   }
-  var el2 = document.getElementsByClassName("no-admin-details");
-  for (y = 0; y < el2.length; y++) {
-    el2[y].closest("details").style.display = "none";
+  var elSection = document.getElementsByClassName("no-admin-section");
+  for (let x = 0; x < elSection.length; x++) {
+    elSection[x].closest("section").style.display = "none";
+  }
+  var elDetails = document.getElementsByClassName("no-admin-details");
+  for (let x = 0; x < elDetails.length; x++) {
+    elDetails[x].closest("details").style.display = "none";
   }
 }
 
 function displayForUser() {
   subtitle.innerHTML = "for Users";
-  var el = document.getElementsByClassName("no-user-section");
-  for (x = 0; x < el.length; x++) {
-    el[x].closest("section").style.display = "none";
+  var el = document.getElementsByClassName("no-user");
+  for (let x = 0; x < el.length; x++) {
+    el[x].style.display = "none";
   }
-  var el2 = document.getElementsByClassName("no-user-details");
-  for (y = 0; y < el2.length; y++) {
-    el2[y].closest("details").style.display = "none";
+  var elSection = document.getElementsByClassName("no-user-section");
+  for (let x = 0; x < elSection.length; x++) {
+    elSection[x].closest("section").style.display = "none";
+  }
+  var elDetails = document.getElementsByClassName("no-user-details");
+  for (let x = 0; x < elDetails.length; x++) {
+    elDetails[x].closest("details").style.display = "none";
   }
 }
 
-if (userRole == "admin") {
-  displayForAdmin()
-} else {
+if (userRole == "user") {
   displayForUser()
+} else {
+  displayForAdmin()
 }
 
 var script = document.getElementsByTagName("script");

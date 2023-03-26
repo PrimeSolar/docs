193ac1a70ed075d2159574aa6c288df392757f2c
Sony <88476416+FirstSolar@users.noreply.github.com>
2023-03-26 19:24:16 +0300

    Create check-browser.js
    Create browser compatibility check script

---
+++ scripts/check-browser.js
@@ -0,0 +1,9 @@
+if (navigator.userAgent.indexOf('Safari') != -1) { //Safari
+  //Permit
+} else if (navigator.userAgent.indexOf('Chrome') != -1) { //Chrome
+  //Permit
+} else {
+  document.body.innerHTML = "<style>body {text-align: center; font-size: 25px;} div {padding-top: 25%;}</style><div>The browser is not supported. Please open this page in another browser.</div>";
+}
+
+console.log("check-browser.js is completed");

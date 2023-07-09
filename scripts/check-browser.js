6202627495ed43609c049c68d6b15eeb70dcdddd
I am <88476416+FirstSolar@users.noreply.github.com>
2023-07-09 19:34:17 +0300

    Update check-browser.js
    Create a comment on the source code

--- scripts/check-browser.js
+++ scripts/check-browser.js
@@ -3,6 +3,7 @@ if (navigator.userAgent.indexOf('Safari') != -1) { //Safari
 } else if (navigator.userAgent.indexOf('Chrome') != -1) { //Chrome
   //Permit
 } else {
+  //Replace document content with a phrase asking to change the browser:
   document.body.innerHTML = "<style>body {text-align: center; font-size: 25px;} div {padding-top: 25%;}</style><div>The browser is not supported. Please open this page in another browser.</div>";
 }
 

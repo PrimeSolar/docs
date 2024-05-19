201c7f6d989267e127c4bcdffabe49ff396a80cc
I am <88476416+FirstSolar@users.noreply.github.com>
2024-05-19 11:43:55 +0300

    Update dark-theme.js
    * Create theme settings for cards.
    * Create theme button style rule analyzing element siblings.

--- scripts/dark-theme.js
+++ scripts/dark-theme.js
@@ -13,16 +13,51 @@ function switchyDarkTheme() {
   if (document.documentElement.classList.contains("dark")) {
     document.documentElement.classList.remove("dark")
     document.documentElement.classList.add("light")
+    for (let x of document.getElementsByClassName("card")) {
+      x.classList.remove("card-dark-theme");
+      x.classList.add("card-light-theme");
+    }
   } else if (document.documentElement.classList.contains("light")) {
     document.documentElement.classList.remove("light")
     document.documentElement.classList.add("dark")
+    for (let x of document.getElementsByClassName("card")) {
+      x.classList.remove("card-light-theme");
+      x.classList.add("card-dark-theme");
+    }
   } else {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
       document.documentElement.classList.add("light")
+      for (let x of document.getElementsByClassName("card")) {
+        x.classList.remove("card-dark-theme");
+        x.classList.add("card-light-theme");
+      }
     } else {
       document.documentElement.classList.add("dark")
+      for (let x of document.getElementsByClassName("card")) {
+        x.classList.remove("card-light-theme");
+        x.classList.add("card-dark-theme");
+      }
     }
   }
 }
+
+const themeButtonNextSibling = document.querySelector("theme-button").nextElementSibling;
+if (themeButtonNextSibling == null || themeButtonNextSibling !== document.querySelector("editor-button")) {
+  document.querySelector("#theme").style.right = "-27px";
+  document.querySelector("#switchyContainerAbsolute").style.right = "0";
+}
+
+function cssHoverEffect() {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    for (let x of document.getElementsByClassName("card")) {
+      x.classList.add("card-dark-theme");
+    }
+  } else {
+    for (let x of document.getElementsByClassName("card")) {
+      x.classList.add("card-light-theme");
+    }
+  }
+}
+cssHoverEffect();
 
 console.log("dark-theme.js is completed");

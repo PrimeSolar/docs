9454439812e21c36c2029346f42246527d243b7c
I am <88476416+FirstSolar@users.noreply.github.com>
2024-08-14 10:13:41 +0300

    Create p-1-line-indent.js
    Create script designed for conditional formatting of web elements.

---
+++ scripts/p-1-line-indent.js
@@ -0,0 +1,12 @@
+// This script is designed for conditional formatting of web elements.
+
+// Select all paragraphs in the document
+const p = document.getElementsByTagName("p");
+// Iterate over each paragraph
+for (let a of p) {
+  // Check if the paragraph is not a descendant of an element with the "text-center" class and doesn't contain this class in itself
+  if (a.closest(".text-center") == null) {
+    // Prepend indentation to the paragraph
+    a.innerHTML = "&nbsp;&nbsp;" + a.innerHTML;
+  }
+}

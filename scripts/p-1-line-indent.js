a58ea439d8689eabc9e8c04df39c913c6cf5b4a7
sony-nike <88476416+sony-nike@users.noreply.github.com>
2024-12-02 22:41:20 +0300

    Update p-1-line-indent.js

--- scripts/p-1-line-indent.js
+++ scripts/p-1-line-indent.js
@@ -7,6 +7,6 @@ for (let a of p) {
   // Check if the paragraph is not a descendant of an element with the "text-center" class and doesn't contain this class in itself
   if (a.closest(".text-center") == null) {
     // Prepend indentation to the paragraph
-    a.innerHTML = "&nbsp;&nbsp;" + a.innerHTML;
+    a.innerHTML = "<indent></indent>" + a.innerHTML;
   }
 }

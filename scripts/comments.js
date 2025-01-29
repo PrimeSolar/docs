e357ef500642fedfb579cb64a06db61c557a5c29
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-01-29 23:21:40 +0300

    Update comments.js
    Update the script that provides the ability to create and view comments in Real&Mate Documentation.

--- scripts/comments.js
+++ scripts/comments.js
@@ -1,8 +1,8 @@
-//Comment
+// Comments
 
-const createCommentary = document.getElementsByTagName('a-comment');
+const createCommentary = document.getElementsByTagName("a-comment");
 const commentaryTag = "<comment-tag onclick='displayCommentary(this)'/>";
-var zIndex = 0;
+let zIndex = 0;
 // Declare a, give it the value of createCommentary
 for (let a of createCommentary) {
   a.insertAdjacentHTML("beforebegin", commentaryTag);
@@ -10,20 +10,21 @@ for (let a of createCommentary) {
 
 function displayCommentary(element) {
   if (element.nextSibling.classList.contains("shown")) {
-    element.nextSibling.classList.remove("shown")
-    element.nextSibling.classList.add("not-shown")
+    element.nextSibling.classList.remove("shown");
+    element.nextSibling.classList.add("not-shown");
     element.nextSibling.style.display = "none";
     element.nextSibling.style.zIndex = "0";
   } else {
-    element.nextSibling.classList.remove("not-shown")
-    element.nextSibling.classList.add("shown")
+    element.nextSibling.classList.remove("not-shown");
+    element.nextSibling.classList.add("shown");
     element.nextSibling.style.display = "block";
     element.nextSibling.style.zIndex = ++zIndex;
   }
   let checkOutElement = element.parentNode.childNodes[0].tagName;
   if (checkOutElement == "COMMENT-LABEL") {
     element.nextSibling.style.transform = "translate(-25px, 30px)";
-  } else {}
+  } else {
+  }
 }
 
 console.log("comments.js is completed");

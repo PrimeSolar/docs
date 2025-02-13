358bf64cde3584943732d25d39da233fd27b6967
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-02-13 23:59:33 +0300

    Update glossary.js

--- scripts/glossary.js
+++ scripts/glossary.js
@@ -6,7 +6,7 @@
 // Terms and Definitions
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////a/////////////////////////////////////////////////////////////////////////////////////////////////
 
 // API Terms
 
@@ -16,7 +16,7 @@ if (gAPI) {
   gAPI.innerHTML += `
   <td>API</td>
   <td><p>Application Programming Interface. It’s a collection of standardized protocols and tools that enable different technology systems to communicate and interact with one another. It serves as an interface that facilitates the access and delivery of data, allowing applications to request and exchange information seamlessly.</p></td>
-  `
+  `;
 }
 
 // Authorization
@@ -25,7 +25,7 @@ if (gAuthorization) {
   gAuthorization.innerHTML += `
   <td>Authorization</td>
   <td><p>Process of specifying access rights/privileges to resources. It identifies what kinds of data a user is allowed to access. To access data through the API, a data requestor supplies an authorization code &mdash; an API key or API token &mdash; and the user’s allowed level of data access is delivered through the API.</p></td>
-  `
+  `;
 }
 
 // Authentication
@@ -34,7 +34,7 @@ if (gAuthentication) {
   gAuthentication.innerHTML += `
   <td>Authentication</td>
   <td><p>The process of verifying a user's identity to ensure that they are who they claim to be. In the context of an API, authentication is essential for determining the legitimacy of a person requesting data, ensuring that only authorized users can access sensitive information.</p></td>
-  `
+  `;
 }
 
 // Data Synchronization
@@ -43,7 +43,7 @@ if (gDataSynchronization) {
   gDataSynchronization.innerHTML += `
   <td>Data Synchronization</td>
   <td><p>The process of copying and synchronizing data between a server and a client. This process involves an initial request to retrieve a dataset from the primary source, followed by subsequent requests to obtain updates. Data synchronization ensures that both server and client maintain consistent and up-to-date information.</p></td>
-  `
+  `;
 }
 
 // Endpoint
@@ -52,7 +52,7 @@ if (gEndpoint) {
   gEndpoint.innerHTML += `
   <td>Endpoint</td>
   <td><p>A web address where data can be accessed. The API service has many endpoints where different kinds of data can be accessed.</p></td>
-  `
+  `;
 }
 
 // HTTP
@@ -61,7 +61,7 @@ if (gHTTP) {
   gHTTP.innerHTML += `
   <td>HTTP</td>
   <td><p>The Hypertext Transfer Protocol. It's the set of rules for transferring data between systems on the World Wide Web or within a local network. HTTP defines how messages are formatted and transmitted, as well as how web servers and browsers should respond to various commands. It is the foundation of data communication on the internet, enabling the retrieval of web pages, images, and other resources, and is commonly used in conjunction with HTTPS (HTTP Secure).</p></td>
-  `
+  `;
 }
 
 // JSON
@@ -70,7 +70,7 @@ if (gJSON) {
   gJSON.innerHTML += `
   <td>JSON</td>
   <td><p>The JavaScript Object Notation. It's a lightweight data-interchange format that is easy for humans to read and write and is easy for machines to parse and generate. Real&Mate’s API can return data to requestors in this format for ease of use.</p></td>
-  `
+  `;
 }
 
 // Metadata
@@ -79,7 +79,7 @@ if (gMetadata) {
   gMetadata.innerHTML += `
   <td>Metadata</td>
   <td><p>Data that provides information about other data. It's a description of what data is available and how it's organized. Metadata can be analyzed so that data requestors understand exactly what the data they access from an API will look like.</p></td>
-  `
+  `;
 }
 
 // OAuth
@@ -88,7 +88,7 @@ if (gOAuth) {
   gOAuth.innerHTML += `
   <td>OAuth</td>
   <td><p>Open authorization. It's an open standard that allows for authentication and authorization in one step. It allows previously-identified users of systems to log in to other APIs and get access to services quickly.</p></td>
-  `
+  `;
 }
 
 // OData
@@ -97,7 +97,7 @@ if (gOData) {
   gOData.innerHTML += `
   <td>OData</td>
   <td><p>Open Data Protocol. It's an ISO/IEC approved, OASIS standard that defines a set of rules for building and using RESTful APIs.</p></td>
-  `
+  `;
 }
 
 // Payload
@@ -106,7 +106,7 @@ if (gPayload) {
   gPayload.innerHTML += `
   <td>Payload</td>
   <td><p>Actual data transmitted as part of a request or response in an API interaction. The payload contains the relevant information that a client sends to a server or a server returns to a client, typically in a structured format such as JSON or XML.</p></td>
-  `
+  `;
 }
 
 // Protocol
@@ -115,7 +115,7 @@ if (gProtocol) {
   gProtocol.innerHTML += `
   <td>Protocol</td>
   <td><p>A set of rules and conventions that govern the format, transmission, and processing of data exchanged between systems. Protocols ensure that data is communicated accurately and efficiently.</p></td>
-  `
+  `;
 }
 
 // Query
@@ -124,7 +124,7 @@ if (gQuery) {
   gQuery.innerHTML += `
   <td>Query</td>
   <td><p>A request made by a user to the API that specifies the type of data needed. It specifies parameters for data retrieval, allowing the API to understand and fulfill user's information requirements effectively.</p></td>
-  `
+  `;
 }
 
 // REST
@@ -132,8 +132,8 @@ const gREST = document.getElementById("gREST");
 if (gREST) {
   gREST.innerHTML += `
   <td>REST</td>
-  <td><p>Representational State Transfer. An architectural style for designing networked applications, particularly in the context of distributed hypermedia systems. REST is based on a set of principles and constraints that promote scalability, stateless communication, and the use of standard HTTP methods (such as GET, POST, PUT, DELETE) for interacting with resources. In a RESTful system, resources are identified by URLs, and the state of the application is represented through the transfer of representations (such as JSON or XML) between the client and server, allowing for a more efficient and flexible interaction model.</p></td>
-  `
+  <td><p>Representational State Transfer. An architectural style for designing networked applications, particularly in the context of distributed hypermedia systems. REST is based on a set of principles and constraints that promote scalability, stateless communication, and the use of standard HTTP methods (such as GET, POST, PUT, DELETE) for interacting with resources. In a RESTful system, resources are identified by URLs, and the state of the application is represented through the transfer of representations (such as JSON or XML) between a client and a server, allowing for a more efficient and flexible interaction model.</p></td>
+  `;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -147,7 +147,7 @@ if (gActiveSolarSystem) {
   gActiveSolarSystem.innerHTML += `
   <td>Active Solar System</td>
   <td><p>A home energy setup that utilizes solar panels to convert sunlight into electricity for immediate use or storage.</p></td>
-  `
+  `;
 }
 
 // Buyers Agency Fee
@@ -156,7 +156,7 @@ if (gBuyersAgencyFee) {
   gBuyersAgencyFee.innerHTML += `
   <td>Buyers Agency Fee</td>
   <td><p>The fee or commission paid to a buyer’s agent or brokerage for finding and managing a home purchase for a buyer. Represented as a percentage of the sales price, the fee is paid by the seller at closing and is intended to compensate the buyer’s agent for their expertise, negotiation skills, and assistance throughout the buying process.</p></td>
-  `
+  `;
 }
 
 // CMA
@@ -165,7 +165,7 @@ if (gCMA) {
   gCMA.innerHTML += `
   <td>CMA</td>
   <td><p>Comparative Market Analysis. It's an in-depth analysis, prepared by a real estate agent, that determines the estimated value of a home based on recently sold homes of similar condition, size, features and age that are located in the same area.</p></td>
-  `
+  `;
 }
 
 // Deed
@@ -174,7 +174,7 @@ if (gDeed) {
   gDeed.innerHTML += `
   <td>Deed</td>
   <td><p>A legal document that formally establishes and transfers ownership of real property from one party to another.</p></td>
-  `
+  `;
 }
 
 // Earnest Money
@@ -183,16 +183,18 @@ if (gEarnestMoney) {
   gEarnestMoney.innerHTML += `
   <td>Earnest Money</td>
   <td><p>A security deposit made by a buyer to demonstrate their serious intent to purchase a property. This deposit is typically submitted along with an offer to buy and serves as a sign of good faith to the seller.</p></td>
-  `
+  `;
 }
 
 // Environmentally Friendly Home Construction
-const gEnvironmentallyFriendlyHomeConstruction = document.getElementById("gEnvironmentallyFriendlyHomeConstruction");
+const gEnvironmentallyFriendlyHomeConstruction = document.getElementById(
+  "gEnvironmentallyFriendlyHomeConstruction"
+);
 if (gEnvironmentallyFriendlyHomeConstruction) {
   gEnvironmentallyFriendlyHomeConstruction.innerHTML += `
   <td>Environmentally Friendly Home Construction</td>
   <td><p>A building constructed with recycled materials and wood from a renewable source. It can also employ many devices and constructs that lessen a carbon footprint, including geothermal heating systems and solar panels.</p></td>
-  `
+  `;
 }
 
 // Infrastructure
@@ -201,7 +203,7 @@ if (gInfrastructure) {
   gInfrastructure.innerHTML += `
   <td>Infrastructure</td>
   <td><p>Organizational structures that are required for society or a business to function.</p></td>
-  `
+  `;
 }
 
 // Installment Contract
@@ -210,7 +212,7 @@ if (gInstallmentContract) {
   gInstallmentContract.innerHTML += `
   <td>Installment Contract </td>
   <td><p>A contractual arrangement in that a buyer agrees to pay the total purchase price of a product or service in a series of scheduled payments, or installments, over a specified period of time. This type of agreement allows the buyer to acquire the item immediately while spreading the cost over time, making it more manageable financially.</p></td>
-  `
+  `;
 }
 
 // Listing Price
@@ -219,7 +221,7 @@ if (gListingPrice) {
   gListingPrice.innerHTML += `
   <td>Listing Price</td>
   <td><p>A price at that a property is offered for sale by a seller. This amount reflects seller's expectations and serves as a starting point for negotiations between a seller and a buyer.</p></td>
-  `
+  `;
 }
 
 // Mortgage Banker
@@ -228,7 +230,7 @@ if (gMortgageBanker) {
   gMortgageBanker.innerHTML += `
   <td>Mortgage Banker</td>
   <td><p>An individual or financial institution that originates, sells, and services mortgage loans. Mortgage bankers provide necessary funds for borrowers to purchase real estate.</p></td>
-  `
+  `;
 }
 
 // Real Estate
@@ -237,5 +239,5 @@ if (gRealEstate) {
   gRealEstate.innerHTML += `
   <td>Real Estate</td>
   <td><p>Any given piece of land along with anything that is permanently attached to it, such as buildings, housing, natural resources, and other permanent structures. Real estate encompasses both residential and commercial properties and is a key component of the property market.</p></td>
-  `
+  `;
 }

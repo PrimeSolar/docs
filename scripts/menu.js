9157aab82f9cb223bb83ff30ce613a4918eb9285
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-04-03 22:54:53 +0300

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,14 +2,25 @@ function menuToggle(x) {
   x.classList.toggle("change");
   document.getElementById("container-button").classList.toggle("show");
 }
-window.onclick = function(event) {
-  if (!event.target.matches('#menu-title') && !event.target.matches('#menu-button') && !event.target.matches('.bar') && !event.target.matches('.dropdown-content') && !event.target.matches('#switchyContainerAbsolute') && !event.target.matches(
-      '#theme') && !event.target.matches(
-      '#switchy') && !event.target.matches('.indicator') && !event.target.matches('#editorModeContainerAbsolute') && !event.target.matches('#edit-doc') && !event.target.matches('#editor-mode') && !event.target.matches('#screenshotBtn')) {
+window.onclick = function (event) {
+  if (
+    !event.target.matches("#menu-title") &&
+    !event.target.matches("#menu-button") &&
+    !event.target.matches(".bar") &&
+    !event.target.matches(".dropdown-content") &&
+    !event.target.matches("#switchyContainerAbsolute") &&
+    !event.target.matches("#theme") &&
+    !event.target.matches("#switchy") &&
+    !event.target.matches(".indicator") &&
+    !event.target.matches("#editorModeContainerAbsolute") &&
+    !event.target.matches("#edit-doc") &&
+    !event.target.matches("#editor-mode") &&
+    !event.target.matches("#screenshotBtn")
+  ) {
     const dropdowns = document.getElementsByClassName("dropdown-content");
     for (let i of dropdowns) {
-      if (i.classList.contains('show')) {
-        i.classList.remove('show');
+      if (i.classList.contains("show")) {
+        i.classList.remove("show");
       }
       const menuButton = document.getElementById("menu-container");
       if (menuButton.classList.contains("change")) {
@@ -17,4 +28,4 @@ window.onclick = function(event) {
       }
     }
   }
-}
+};

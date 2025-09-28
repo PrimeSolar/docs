128198a4d3a09f947bbe096916dabd795b016497
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-09-29 02:05:11 +0300

    Update editor-mode.js

--- scripts/editor-mode.js
+++ scripts/editor-mode.js
@@ -1,26 +1,21 @@
 function editorMode() {
-  if (document.documentElement.classList.contains("editor-mode-on")) {
-    document.documentElement.classList.remove("editor-mode-on");
-    document.documentElement.classList.add("editor-mode-off");
-    document
-      .getElementById("body")
-      .attributes.removeNamedItem("contenteditable");
-  } else {
-    document.documentElement.classList.remove("editor-mode-off");
-    document.documentElement.classList.add("editor-mode-on");
-    document.getElementById("body").setAttribute("contenteditable", "true");
-    if (document.documentElement.classList.contains("editor-mode-on-alerted")) {
-    } else {
-      alert(
-        "Now you can edit this document in your browser.\nIMPORTANT: changes will not be saved on the server.\nYou may save edited document on your device as an HTML page for later usage."
-      );
-      document.documentElement.classList.add("editor-mode-on-alerted");
-    }
-  }
+  const body = document.querySelector("body");
+  body.classList.contains("editor-mode-on")
+    ? (body.classList.remove("editor-mode-on"),
+      body.classList.add("editor-mode-off"),
+      body.attributes.removeNamedItem("contenteditable"))
+    : (body.classList.remove("editor-mode-off"),
+      body.classList.add("editor-mode-on"),
+      body.setAttribute("contenteditable", "true"),
+      body.classList.contains("editor-mode-on-alerted") ||
+        (alert(
+          "Now you can edit this document in your browser.\nIMPORTANT: changes will not be saved on the server.\nYou may save edited document on your device as an HTML page for later usage."
+        ),
+        body.classList.add("editor-mode-on-alerted")));
 }
 
-const constEditorMode = document.getElementById("editor-mode");
+const editorModeButton = document.getElementById("editor-mode");
 function moveIndicator() {
-  constEditorMode.classList.toggle("editIsActive");
+  editorModeButton.classList.toggle("editIsActive");
 }
-constEditorMode.addEventListener("click", moveIndicator, false);
+editorModeButton.addEventListener("click", moveIndicator, !1);

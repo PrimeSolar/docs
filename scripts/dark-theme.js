710927215f1bf23d21dc25720592d0a47eab9290
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-10-26 21:04:24 +0300

    Update dark-theme.js

--- scripts/dark-theme.js
+++ scripts/dark-theme.js
@@ -1,6 +1,26 @@
-// Click tumbler to toggle .light and .dark classes.
-// If class is not presented, determines actual condition based on the OS or web browser color scheme.
-// If OS and web browser color scheme aren't supported, determines light condition.
+/*
+ * Theme Management Script
+ *
+ * This script handles the theme management of the Real&Mate Documentation web application.
+ * It allows to toggle between themes and applies a selected mode
+ * to a webpage, improving user experience and
+ * ensuring the interface is visually appealing.
+ *
+ * Click tumbler to toggle "light" and "dark" classes.
+ * If a class is not presented, determines prefered theme based on the OS or web browser color scheme.
+ * If OS and web browser color scheme aren't supported, it sets the light theme.
+ *
+ * Copyright © Vladislav Kazantsev
+ * All rights reserved.
+ * This code is the intellectual property of Vladislav Kazantsev.
+ * You are welcome to clone the related repository and use the code for exploratory purposes.
+ * However, unauthorized reproduction, modification, or redistribution of this code (including cloning of related repository or altering it for activities beyond exploratory use) is strictly prohibited.
+ * Code snippets may be shared only when the original author is explicitly credited and a direct link to the original source of the code is provided alongside the code snippet.
+ * Sharing the link to the file is permitted, except when directed toward retrieval purposes.
+ * Any form of interaction with this file is strictly prohibited when facilitated by the code, except when such interaction is for discussion or exchange purposes with others.
+ * This copyright notice applies globally.
+ * For inquiries about collaboration, usage outside exploratory purposes, or permissions, please contact: hypervisor7@pm.me
+ */
 
 const switchy = document.getElementById("switchy");
 

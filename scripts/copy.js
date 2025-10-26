fdfab8d6039d91ceb3d63f572ca5ca81b7849766
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-10-26 21:03:51 +0300

    Update copy.js

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,3 +1,26 @@
+/*
+ * Copy Button Functionality for Code Blocks
+ *
+ * This script loops through all <codeblock> elements on the page
+ * and adds a "Copy" button to each one. When clicked, the button
+ * calls the "copy" function, that copies the text content of
+ * the corresponding code block to the clipboard. The button also
+ * provides user feedback by changing its text to "Copied!" for
+ * one second, then reverting back to "Copy". This enhances
+ * user interaction and simplifies the process of copying code snippets.
+ *
+ * Copyright © Vladislav Kazantsev
+ * All rights reserved.
+ * This code is the intellectual property of Vladislav Kazantsev.
+ * You are welcome to clone the related repository and use the code for exploratory purposes.
+ * However, unauthorized reproduction, modification, or redistribution of this code (including cloning of related repository or altering it for activities beyond exploratory use) is strictly prohibited.
+ * Code snippets may be shared only when the original author is explicitly credited and a direct link to the original source of the code is provided alongside the code snippet.
+ * Sharing the link to the file is permitted, except when directed toward retrieval purposes.
+ * Any form of interaction with this file is strictly prohibited when facilitated by the code, except when such interaction is for discussion or exchange purposes with others.
+ * This copyright notice applies globally.
+ * For inquiries about collaboration, usage outside exploratory purposes, or permissions, please contact: hypervisor7@pm.me
+ */
+
 // Loop through each codeblock and add "Copy" buttons to them
 const codeblocks = document.querySelectorAll("codeblock");
 codeblocks.forEach((codeblock) => {

8e04db3807e5a74769c1c8caf8777fdb2003e5ec
I am <88476416+PrimeSolar@users.noreply.github.com>
2026-03-09 00:30:15 +0300

    Wrap the functionality in DOMContentLoaded event listener
    * Wrap the functionality in DOMContentLoaded event listener to ensure that DOM elements exist before accessing them.
    * Improve the introductory commentary of the source code for better readability, maintenance, precision, consistency, and clarity.

--- scripts/screenshot.js
+++ scripts/screenshot.js
@@ -1,5 +1,14 @@
 /**
- * Script
+ * Script for Downloading a Webpage as an Image
+ *
+ * Capture a screenshot of the current page and download it as an image.
+ *
+ * Dynamically loads the script and appends it to a webpage,
+ * applies visual and layout styles suitable for printing of various element groups,
+ * renders document.body to a canvas, converts the canvas to a JPEG data URL,
+ * creates an anchor with the data URL and a download attribute with the filename,
+ * programmatically clicks it to trigger the file download,
+ * and applies the original visual and layout styles to the element groups.
  *
  * Copyright © Vladislav Kazantsev
  * All rights reserved.
@@ -13,110 +22,112 @@
  * For inquiries about collaboration, usage outside exploratory purposes, or permissions, please contact: hypervisor7@pm.me
  */
 
-const html2CanvasScript = document.createElement("script");
-html2CanvasScript.type = "text/javascript";
-html2CanvasScript.src =
-  "https://html2canvas.hertzen.com/dist/html2canvas.min.js";
-document.querySelector("body").appendChild(html2CanvasScript);
+document.addEventListener("DOMContentLoaded", () => {
+  const html2CanvasScript = document.createElement("script");
+  html2CanvasScript.type = "text/javascript";
+  html2CanvasScript.src =
+    "https://html2canvas.hertzen.com/dist/html2canvas.min.js";
+  document.querySelector("body").appendChild(html2CanvasScript);
 
-const containerButton = document.getElementById("container-button");
-const dropdown = document.getElementById("dropdown");
-const titleClass = document.querySelectorAll(".title");
-const logoContainer = document.querySelector("logo-img");
-const localReusableItems = document.getElementById("local-reusable-items");
-const a = document.getElementsByTagName("a");
-const button = document.getElementsByTagName("button");
-const speakButtons = document.getElementsByClassName("speak-inline");
-const comment = document.getElementsByTagName("comment-tag");
-const noscript = document.getElementsByTagName("noscript");
-const switchyBtn = document.getElementById("switchy");
+  const containerButton = document.getElementById("container-button");
+  const screenshotButton = document.querySelector("#screenshotBtn");
+  const dropdown = document.getElementById("dropdown");
+  const titleClass = document.querySelectorAll(".title");
+  const logoContainer = document.querySelector("logo-img");
+  const localReusableItems = document.getElementById("local-reusable-items");
+  const a = document.getElementsByTagName("a");
+  const button = document.getElementsByTagName("button");
+  const speakButtons = document.getElementsByClassName("speak-inline");
+  const comment = document.getElementsByTagName("comment-tag");
+  const noscript = document.getElementsByTagName("noscript");
 
-function screenshot() {
-  containerButton.style.display = "none";
-  dropdown.style.display = "none";
-  if (localReusableItems != null) {
-    localReusableItems.style.display = "none";
-  }
-  if (logoContainer) {
-    logoContainer.style.animation = "none";
-  }
-  for (let x of titleClass) {
-    x.style.background = "none";
-    x.style.backgroundClip = "none";
-    x.style.webkitTextFillColor = "none";
-    x.style.animation = "none";
-    x.style.color = "#007fff";
-  }
-  for (let x of a) {
-    x.style.textDecoration = "none";
-  }
-  for (let x of button) {
-    x.style.display = "none";
-  }
-  for (let x of speakButtons) {
-    x.style.display = "none";
-  }
-  for (let x of comment) {
-    x.style.display = "none";
-  }
-  for (let x of noscript) {
-    x.style.display = "none";
-  }
-  html2canvas(document.body)
-    .then((canvas) => {
-      const dataURL = canvas.toDataURL("image/jpeg");
-      const link = document.createElement("a");
-      link.setAttribute("download", "screenshot.jpg");
-      link.setAttribute("href", dataURL);
-      link.click();
-    })
-    .catch((error) => {
-      console.error("Error capturing screenshot:", error);
-    });
-  containerButton.style.display = null;
-  for (let x of titleClass) {
-    x.style.background = "linear-gradient(to right, #007fff, #5597ff)";
-    x.style.backgroundClip = "text";
-    x.style.webkitTextFillColor = "transparent";
-    x.style.animation = "brighten 1s";
-    x.style.color = "none";
-  }
-  dropdown.style.display = null;
-  for (let x of a) {
-    x.style.textDecoration = "underline";
-  }
-  for (let x of button) {
-    x.style.display = "block";
-  }
-  for (let x of speakButtons) {
-    x.style.display = "inline-block";
-  }
-  for (let x of comment) {
-    x.style.display = "inline";
-  }
-}
-
-function themeScreenshot() {
-  switchyBtn.click();
-  screenshot();
-  switchyBtn.click();
-}
-
-screenshotBtn.addEventListener("click", () => {
-  if (document.documentElement.classList.contains("dark")) {
-    themeScreenshot();
-  } else if (document.documentElement.classList.contains("light")) {
-    screenshot();
-  } else {
-    if (
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      themeScreenshot();
-    } else {
-      screenshot();
+  function screenshot() {
+    containerButton.style.display = "none";
+    dropdown.style.display = "none";
+    if (localReusableItems != null) {
+      localReusableItems.style.display = "none";
+    }
+    if (logoContainer) {
+      logoContainer.style.animation = "none";
+    }
+    for (let x of titleClass) {
+      x.style.background = "none";
+      x.style.backgroundClip = "none";
+      x.style.webkitTextFillColor = "none";
+      x.style.animation = "none";
+      x.style.color = "#007fff";
+    }
+    for (let x of a) {
+      x.style.textDecoration = "none";
+    }
+    for (let x of button) {
+      x.style.display = "none";
+    }
+    for (let x of speakButtons) {
+      x.style.display = "none";
+    }
+    for (let x of comment) {
+      x.style.display = "none";
+    }
+    for (let x of noscript) {
+      x.style.display = "none";
+    }
+    html2canvas(document.body)
+      .then((canvas) => {
+        const dataURL = canvas.toDataURL("image/jpeg");
+        const link = document.createElement("a");
+        link.setAttribute("download", "screenshot.jpg");
+        link.setAttribute("href", dataURL);
+        link.click();
+      })
+      .catch((error) => {
+        console.error("Error capturing screenshot:", error);
+      });
+    containerButton.style.display = null;
+    for (let x of titleClass) {
+      x.style.background = "linear-gradient(to right, #007fff, #5597ff)";
+      x.style.backgroundClip = "text";
+      x.style.webkitTextFillColor = "transparent";
+      x.style.animation = "brighten 1s";
+      x.style.color = "none";
+    }
+    dropdown.style.display = null;
+    for (let x of a) {
+      x.style.textDecoration = "underline";
+    }
+    for (let x of button) {
+      x.style.display = "block";
+    }
+    for (let x of speakButtons) {
+      x.style.display = "inline-block";
+    }
+    for (let x of comment) {
+      x.style.display = "inline";
     }
   }
-});
 
-console.log("screenshot.js is completed");
+  function themeScreenshot() {
+    switchy.click();
+    screenshot();
+    switchy.click();
+  }
+
+  screenshotButton.addEventListener("click", () => {
+    if (document.documentElement.classList.contains("dark")) {
+      themeScreenshot();
+    } else if (document.documentElement.classList.contains("light")) {
+      screenshot();
+    } else {
+      if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+      ) {
+        themeScreenshot();
+      } else {
+        screenshot();
+      }
+    }
+  });
+
+  console.log("screenshot.js is completed");
+});

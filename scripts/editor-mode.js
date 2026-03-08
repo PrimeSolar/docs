64c4e4fe6b182c83c0b746b9aff51998bd891f24
I am <88476416+PrimeSolar@users.noreply.github.com>
2026-03-09 00:08:04 +0300

    Wrap the functionality in DOMContentLoaded event listener
    * Wrap the functionality in DOMContentLoaded event listener to ensure that DOM elements exist before accessing them.
    * Improve the introductory commentary of the source code for better readability, maintenance, precision, consistency, and clarity.

--- scripts/editor-mode.js
+++ scripts/editor-mode.js
@@ -1,5 +1,7 @@
 /**
- * Script
+ * The Script for In-Browser Document Editing
+ *
+ * This script enables a toggle function for editing mode in a web document. When activated, users can edit the document directly in their browser. The script provides visual feedback by alerting the user about the editing. It enhances user interaction and editing capabilities with the functionality for seamless document management.
  *
  * Copyright © Vladislav Kazantsev
  * All rights reserved.
@@ -13,24 +15,28 @@
  * For inquiries about collaboration, usage outside exploratory purposes, or permissions, please contact: hypervisor7@pm.me
  */
 
-function editorMode() {
-  const body = document.querySelector("body");
-  body.classList.contains("editor-mode-on")
-    ? (body.classList.remove("editor-mode-on"),
-      body.classList.add("editor-mode-off"),
-      body.attributes.removeNamedItem("contenteditable"))
-    : (body.classList.remove("editor-mode-off"),
-      body.classList.add("editor-mode-on"),
-      body.setAttribute("contenteditable", "true"),
-      body.classList.contains("editor-mode-on-alerted") ||
-        (alert(
-          "Now you can edit this document in your browser.\nIMPORTANT: changes will not be saved on the server.\nYou may save edited document on your device as an HTML page for later usage."
-        ),
-        body.classList.add("editor-mode-on-alerted")));
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const editorModeButton = document.getElementById("editor-mode");
 
-const editorModeButton = document.getElementById("editor-mode");
-function moveIndicator() {
-  editorModeButton.classList.toggle("editIsActive");
-}
-editorModeButton.addEventListener("click", moveIndicator, !1);
+  function editorMode() {
+    const body = document.querySelector("body");
+    body.classList.contains("editor-mode-on")
+      ? (body.classList.remove("editor-mode-on"),
+        body.classList.add("editor-mode-off"),
+        body.attributes.removeNamedItem("contenteditable"))
+      : (body.classList.remove("editor-mode-off"),
+        body.classList.add("editor-mode-on"),
+        body.setAttribute("contenteditable", "true"),
+        body.classList.contains("editor-mode-on-alerted") ||
+          (alert(
+            "Now you can edit this document in your browser.\nIMPORTANT: changes will not be saved on the server.\nYou may save edited document on your device as an HTML page for later usage."
+          ),
+          body.classList.add("editor-mode-on-alerted")));
+  }
+  editorModeButton.addEventListener("click", editorMode);
+
+  function moveIndicator() {
+    editorModeButton.classList.toggle("editIsActive");
+  }
+  editorModeButton.addEventListener("click", moveIndicator, !1);
+});
